490b5a6291eef6ff64beca19b55e1856fe32325c
Тимошенко Антон Иванович <SierrA_931@outlook.com>
2019-12-11 01:50:56 +0300

    Добавьте файлы проекта.

---
+++ SortInsertable/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace SortInsertable {
+    class Program {
+        static void Main() {
+            int[] arr = { 2, 2, 1, 5, 6, 9 };
+            int x, i, j;
+            for (i = 0; i < arr.Length; i++) {
+                x = arr[i];
+                for (j = i - 1; j >= 0 && arr[j] > x; j--)
+                    arr[j + 1] = arr[j];
+                arr[j + 1] = x;
+            }
+            for (int z = 0; z < arr.Length; z++) {
+                Console.WriteLine(arr[z]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
